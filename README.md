a184a798c2679900ddb2be713c514558eb214f7f
brapoprod <broels@chili-publish.com>
2022-12-21 16:13:36 +0100

    First commit with all included

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # jira-release-from-ticketumbers-action
 This action will allow you to create a JIRA release by a list of ticketnumbers that are provided
+
+## Inputs
+
+### `jiraUrl`
+**Required** The base URL for your JIRA installment
+
+### `jiraPAT`
+**Required** Personal access token with read and write access to the JIRA project you want to access. If you need to know how to create and manage these, go to [the following link](https://confluence.atlassian.com/enterprise/using-personal-access-tokens-1026032365.html)
+
+### `jiraProjectKey`
+**Required** The key of the project you want to use f.e. `PRJ` which is also the default.
+
+### `jiraProjectId`
+**Required** The ID of the project you want to use f.e. `1234` which is also the default
+
+### `releaseName`
+The name that you want to give the release in JIRA
+
+### `releaseDescription`
+The description you want to give the release in JIRA
+
+### `jiraTicketIds`
+**Required** An array of tickets that need to be included in the release f.e. ["PRJ-1", "PRJ-2"].
+If you want to retrieve this from the PRs in a github release, you can use [this action](https://github.com/brapoprod/get-jira-tickets-from-github-release-action).
---
+++ index.js
@@ -0,0 +1,67 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const axios = require("axios");
+
+try {
+  // Replace with your JIRA base URL, username, and API token
+  const JIRA_URL = core.getInput("jiraUrl");
+  const JIRA_AUTH_TOKEN = core.getInput("jiraPAT");
+
+  // Replace with the ID of the project you want to create a release for
+  const PROJECT_KEY = core.getInput("jiraProjectKey");
+  const PROJECT_ID = core.getInput("jiraProjectId");
+
+  // Replace with the desired name and description for the release
+  const RELEASE_NAME = core.getInput("releaseName");
+  const RELEASE_DESCRIPTION = core.getInput("releaseDescription");
+
+  // Set the endpoint for creating a version in JIRA
+  const CREATE_VERSION_ENDPOINT = `${JIRA_URL}/rest/api/2/version`;
+
+  // Set the payload with the release name and description
+  const payload = {
+    name: RELEASE_NAME,
+    description: RELEASE_DESCRIPTION,
+    project: PROJECT_KEY,
+    projectId: PROJECT_ID,
+  };
+
+  // Send a POST request to the endpoint to create the release
+  axios
+    .post(CREATE_VERSION_ENDPOINT, payload, {
+      headers: { Authorization: `Bearer ${JIRA_AUTH_TOKEN}` },
+    })
+    .then(async (response) => {
+      // If the request is successful, the release was created
+      console.log(
+        `Release successfully created ${JSON.stringify(response.data)}`
+      );
+
+      const newVersionId = response.data.id;
+      const CHANGE_ISSUE_ENDPOINT = `${JIRA_URL}/rest/api/2/issue`;
+
+      const ticketIds = core.getInput("jiraTicketIds");
+
+      ticketIds.forEach(async (ticketId) => {
+        const ticketPayload = {
+          update: {
+            fixVersions: [{ add: { id: newVersionId } }],
+          },
+        };
+        const ticketsAdded = await axios.put(
+          `${CHANGE_ISSUE_ENDPOINT}/${ticketId}`,
+          ticketPayload,
+          {
+            headers: { Authorization: `Bearer ${JIRA_AUTH_TOKEN}` },
+          }
+        );
+        console.log(ticketsAdded.status);
+      });
+    })
+    .catch((error) => {
+      // If the request is unsuccessful, print the error message
+      console.log(
+        `Error creating release: ${JSON.stringify(error.response.data)}`
+      );
+    });
+} catch (error) {}
